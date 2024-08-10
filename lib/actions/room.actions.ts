3476ea51cc2e14c4d85bc14a00c0e547685df3be
AlamGarry <alqifariq@gmail.com>
2024-08-10 12:19:28 +0700

    fix change

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -4,6 +4,7 @@ import { nanoid } from 'nanoid';
 import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
 import { getAccessType, parseStringify } from '../utils';
+import { redirect } from 'next/navigation';
 export const createDocuments = async ({
   userId,
   email,
@@ -73,12 +74,6 @@ export const getDocuments = async (email: string) => {
   try {
     const rooms = await liveblocks.getRooms({ userId: email });
 
-    // TODO: Bring this back when we have the permission system in place
-    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
-
-    // if (!hasAccess) {
-    //   throw new Error('You do not have access to this document');
-    // }
     return parseStringify(rooms);
   } catch (error) {
     console.log(`Error happend while getting a rooms: ${error}`);
@@ -99,7 +94,21 @@ export const updateDocumentAccess = async ({
       usersAccesses,
     });
     if (room) {
-      // TODO: Send a notification to other user
+      const notificationId = nanoid();
+
+      await liveblocks.triggerInboxNotification({
+        userId: email,
+        kind: '$documentAccess',
+        subjectId: notificationId,
+        activityData: {
+          userType,
+          title: `You have been granted ${userType} access to the document by ${updatedBy.name}`,
+          updatedBy: updatedBy.name,
+          avatar: updatedBy.avatar,
+          email: updatedBy.email,
+        },
+        roomId,
+      });
     }
 
     revalidatePath(`/documents/${roomId}`);
@@ -131,5 +140,15 @@ export const removeCollaborator = async ({
     return parseStringify(updateRoom);
   } catch (error) {
     console.log(`Error happend while removing a room: ${error}`);
+  }
+};
+
+export const deleteDocument = async (roomId: string) => {
+  try {
+    await liveblocks.deleteRoom(roomId);
+    revalidatePath('/');
+    redirect('/');
+  } catch (error) {
+    console.log(`Error happend while deleting a room: ${error}`);
   }
 };
